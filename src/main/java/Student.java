fa4c802875556a19e22c523540a12ce3af8e580f
Parth Patel <pate5160@shernet.sheridancollege.ca>
2023-05-16 09:55:46 -0400

    please commit

---
+++ src/main/java/Student.java
@@ -0,0 +1,21 @@
+public class Student {
+    private string sname;
+    private string sID;
+
+    public string getSname() {
+        return sname;
+    }
+
+    public void setSname(string sname) {
+        this.sname = sname;
+    }
+
+    public string getsID() {
+        return sID;
+    }
+
+    public void setsID(string sID) {
+        this.sID = sID;
+    }
+    
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+public class studentlist {
+    
+    public static void main (String[] args);
+    Student s1=new Student(sID: "s1", sname:"Parth;");
+    Student[] studentlist = new student[3];
+    studentlist[0]=s1;
+    studentlist[1]=new Student(sId:"s2",sname: "parth");
+            
+    studentlist[2]=new Student    (sId:"s3",sname:"patel");
+}
